77e333ea383dd2be1bc740f30bab949b69eae6e4
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-10-11 18:23:31 -0300

    exercicio 5 part 1 finished

---
+++ src/exercicio5/Aplication.java
@@ -0,0 +1,7 @@
+package exercicio5;
+
+public class Aplication {
+    public void run() {
+
+    }
+}
---
+++ src/exercicio5/Principal.java
@@ -0,0 +1,8 @@
+package exercicio5;
+
+public class Principal {
+    public static void main(String[] args) {
+        Aplication aplication = new Aplication();
+        aplication.run();
+    }
+}
